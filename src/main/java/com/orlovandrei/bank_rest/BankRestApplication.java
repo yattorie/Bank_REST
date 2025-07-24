da0778f9ec916453c678c249abb6149e992ba0fa
yattorie <pyryryqw882@gmail.com>
2025-07-24 16:35:12 +0300

    Initial commit

---
+++ src/main/java/com/orlovandrei/bank_rest/BankRestApplication.java
@@ -0,0 +1,13 @@
+package com.orlovandrei.bank_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orlovandrei/bank_rest/BankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlovandrei.bank_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
